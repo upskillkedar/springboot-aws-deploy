f8c65c60045083fc4ab88a1e0c4f6febabfa6f1e
Kedar <upskillkedar@gmail.com>
2024-06-14 15:28:42 -0400

    buildspec added

---
+++ src/main/java/com/example/springboot_aws_deploy/TestController.java
@@ -0,0 +1,15 @@
+package com.example.springboot_aws_deploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class TestController {
+
+    @GetMapping("/data")
+    public String getData() {
+        return "First Message from AWS ECS";
+    }
+}
