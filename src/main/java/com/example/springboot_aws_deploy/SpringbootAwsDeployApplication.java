4dbdb12c73eb183198c9b3b4840491d68083b6ca
Kedar <upskillkedar@gmail.com>
2024-06-14 14:47:28 -0400

    FirstMsg

---
+++ src/main/java/com/example/springboot_aws_deploy/SpringbootAwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_aws_deploy/SpringbootAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
